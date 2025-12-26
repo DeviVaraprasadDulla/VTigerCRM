f7e0173f2d5e4a1b51455cbec0af3f5339b603f1
Undekode Venkatesh <venkatesh9949.u@gmail.com>
2025-12-26 12:49:18 +0530

    Created one test case for new organization creating with Last name

--- VTigerCRM/src/test/java/com/vtiger/crm/organization/CreatingOrgWithLastName.java
+++ VTigerCRM/src/test/java/com/vtiger/crm/organization/CreatingOrgWithLastName.java
@@ -1,5 +1,41 @@
 package com.vtiger.crm.organization;
 
-public class CreatingOrgWithLastName {
+import org.testng.annotations.Test;
 
+import com.vtiger.crm.baseclass.BaseClass;
+import com.vtiger.crm.generic.fileUtility.XL_Utility;
+import com.vtiger.crm.generic.webDriverUtility.JavaUtility;
+import com.vtiger.generic.ObjectRepository.HomePage;
+import com.vtiger.generic.ObjectRepository.NewOrganizationCreationPage;
+import com.vtiger.generic.ObjectRepository.OrganizationCreatingConformationPage;
+import com.vtiger.generic.ObjectRepository.OrganizationPage;
+
+public class CreatingOrgWithLastName extends BaseClass{
+
+	@Test
+	public void CreateOrganizationTest() throws Exception {
+		
+		XL_Utility xu = new XL_Utility();
+		JavaUtility ju = new JavaUtility();
+		HomePage hp = new HomePage(driver);
+		OrganizationPage op = new OrganizationPage(driver);
+		NewOrganizationCreationPage n = new NewOrganizationCreationPage(driver);
+		
+		hp.getOrganizationLink().click();
+		
+		op.getNewOrgLink().click();
+		
+		String orgname =xu.getDataFromExcel("org", 1, 2).toString()+ju.getRandomNumber();
+		String shippingAddress = xu.getDataFromExcel("org", 1, 2).toString();
+
+		n.createOrg("Mahi", "Mahi");
+		
+		Thread.sleep(2000);
+		String header =new OrganizationCreatingConformationPage(driver).getHeader().getText();
+		if(header.contains(orgname)) {
+			System.out.println(orgname +" is created == pass");
+		}else {
+			System.out.println(orgname+" is not created == Fail");
+		}
+	}
 }
